5f5ac6c008538224980e8f80a0e5d7b6ddde25a1
LuisFernandoCadimaCampos <nando1296@gmail.com>
2023-11-20 00:56:27 -0400

    Vista Vehiculos

--- frontweb/src/pages/vehiculos.js
+++ frontweb/src/pages/vehiculos.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react'
+import './vehiculos.css';
 
 export default class Vehiculos extends Component {
   render() {
     return (
-      <div>
-        vehiculos
+      <div class="vehiculos">
+          <div class="contenedor-vehiculos">
+
+            <div class="card">
+              <div class="textos">
+                <h3>Datos del vehículo</h3>
+                <p>Marca:</p>
+                <p>Tipo:</p>
+                <p>Costo:</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="textos">
+                <h3>Datos del vehículo</h3>
+                <p>Marca:</p>
+                <p>Tipo:</p>
+                <p>Costo:</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="textos">
+                <h3>Datos del vehículo</h3>
+                <p>Marca:</p>
+                <p>Tipo:</p>
+                <p>Costo:</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="textos">
+                <h3>Datos del vehículo</h3>
+                <p>Marca:</p>
+                <p>Tipo:</p>
+                <p>Costo:</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="textos">
+                <h3>Datos del vehículo</h3>
+                <p>Marca:</p>
+                <p>Tipo:</p>
+                <p>Costo:</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="textos">
+                <h3>Datos del vehículo</h3>
+                <p>Marca:</p>
+                <p>Tipo:</p>
+                <p>Costo:</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="ml-auto">
+              <button id="btn4" className="btn4 btn-primary" type="">Siguiente</button>
+          </div>
       </div>
     )
   }
 }
+
+
